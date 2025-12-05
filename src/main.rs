b81da06e1765702a7d28e9225a6df6f7d2ac6dbe
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2025-12-05 16:31:12 +0100

    Working, 'payload' is supposed to be the path when f=100

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
 use clap::Parser;
 use std::io::{self, Write};
 use base64::{prelude::BASE64_STANDARD, Engine};
-//use termion::raw::IntoRawMode;
-//use std::io::{Write, stdout};
-//use crossterm::{ terminal::{disable_raw_mode, enable_raw_mode, },};
 
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
@@ -16,11 +13,9 @@ pub struct Args {
 fn main() -> Result<(), std::io::Error> {
     let args = Args::parse();
 
-    //let prefix = b"\033_G";
-    let control_data = b"f=100;";
-    //let delimiter = b";";
-    let payload = std::fs::read(args.file)?;
-    //let suffix = b"\033\\";
+    let control_data = b"f=100,t=f,a=T;";
+    //let payload = std::fs::read(args.file)?;
+    let payload = args.file.as_bytes().to_vec();
 
     let prefix = b"\x1b_G";
     let suffix = b"\x1b\\";
@@ -28,20 +23,12 @@ fn main() -> Result<(), std::io::Error> {
     let mut out_buf: Vec<u8> = vec![];
     out_buf.extend(prefix);
     out_buf.extend(control_data);
-    //out_buf.extend(delimiter);
     out_buf.extend(BASE64_STANDARD.encode(payload).as_bytes());
     out_buf.extend(suffix);
-
-    //enable_raw_mode()?;
 
     let mut stdout = io::stdout();
     stdout.write_all(&out_buf)?;
     stdout.flush()?;
-
-    //disable_raw_mode()?;
-    // From termion
-    //let mut stdout = stdout().into_raw_mode()?;
-    //write!(stdout, "{out_buf:?}")?;
 
     Ok(())
 }
