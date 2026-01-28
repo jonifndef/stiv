7fa1678ca47611136970b76b0d57e712b96a9173
Jonas Fredriksson <jonas.fredriksson92@gmail.com>
2026-01-28 20:16:42 +0100

    Add support for tmux (kinda) in the raw-pillow.py script

--- tools/pillow-raw.py
+++ tools/pillow-raw.py
@@ -3,22 +3,27 @@ import sys
 import base64
 from PIL import Image
 
+def tmux_wrap(payload: bytes) -> bytes:
+    return b"\x1bPtmux;" + payload.replace(b"\x1b", b"\x1b\x1b") + b"\x1b\\"
+
 def main(path):
     img = Image.open(path).convert("RGB")
     width, height = img.size
     raw = img.tobytes()
     encoded = base64.b64encode(raw)
-
     CHUNK = 4096
+
     for i in range(0, len(encoded), CHUNK):
         chunk = encoded[i:i+CHUNK]
         m = 1 if i + CHUNK < len(encoded) else 0
-        sys.stdout.buffer.write(
+
+        kitty_msg = (
             f"\x1b_G{'a=T,' if i == 0 else ''}"
-            f"f=24,s={width},v={height},m={m};".encode("ascii")
-        )
-        sys.stdout.buffer.write(chunk)
-        sys.stdout.buffer.write(b"\x1b\\")
+            f"f=24,s={width},v={height},m={m};"
+        ).encode("ascii") + chunk + b"\x1b\\"
+
+        #sys.stdout.buffer.write(tmux_wrap(kitty_msg))
+        sys.stdout.buffer.write(kitty_msg)
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
